7713e36d6081a8563459d0e020ff54c0b885f58f
valkigman <kvale8@gmail.com>
2022-06-30 15:21:57 -0700

    44

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
+
 int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle");
-	getchar();
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
     
