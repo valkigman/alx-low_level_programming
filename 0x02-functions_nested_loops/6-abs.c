738873a5766c3623b8d4a216a68dcb34d446f702
valkigman <kvale8@gmail.com>
2022-07-05 17:07:56 -0700

    66

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of int
+ *
+ * @a: as int
+ *
+ * Return: abs value
+ */
+int _abs(int a)
+{
+	if (a >= 0)
+	{
+		return (a);
+	}
+	else
+	{
+		return (a * -1);
+	}
+}
