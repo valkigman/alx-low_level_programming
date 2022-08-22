a680724dac3d9a0c1ad2d54e54126b66877a6fce
valkigman <kvale8@gmail.com>
2022-08-22 11:08:34 -0700

    program that displays the information contained in the ELF header at the start of an ELF file.

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * main - copies the content of a file to another file
+ * @ac: argument count
+ * @av: argument values
+ * Return: 0 on success, -1 on error
+ */
+int main(int ac, char *av[])
+{
+	return (1);
+	return (0);
+}
